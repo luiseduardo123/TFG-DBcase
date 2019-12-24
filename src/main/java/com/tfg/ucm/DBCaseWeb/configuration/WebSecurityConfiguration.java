5e754441e17259116a02803b1921d57ffa4fdb3d
luiseduardo123 <lalin_200@hotmail.com>
2019-12-24 17:46:11 +0100

    Login OK

---
+++ src/main/java/com/tfg/ucm/DBCaseWeb/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.tfg.ucm.DBCaseWeb.configuration;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableOAuth2Sso
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+   @Override
+   protected void configure(HttpSecurity http) throws Exception {
+	   http
+       .csrf()
+       .disable()
+       .authorizeRequests()
+       .antMatchers("/", "/index.html", "/**/*.png", "/**/*.css", "/**/*.js", "/**/*.ttf", "/**/*.jpg")
+       .permitAll()
+       .anyRequest()
+       .authenticated()
+       .and()
+       .formLogin()
+       .loginPage("/login1.html")
+       .loginProcessingUrl("/perform_login1")
+       .defaultSuccessUrl("/homepage.html", true)
+       .and()
+       .logout().invalidateHttpSession(true)
+       .clearAuthentication(true)
+       .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+       .logoutSuccessUrl("/logout-success");
+   }
+}
