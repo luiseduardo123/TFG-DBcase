00b381f229e82613e7ee755e91f57b8db2f08d9b
luiseduardo123 <lalin_200@hotmail.com>
2019-12-24 18:03:56 +0100

    * ok

--- src/main/java/com/tfg/ucm/DBCaseWeb/configuration/GoogleserviceApplication.java
+++ src/main/java/com/tfg/ucm/DBCaseWeb/configuration/GoogleserviceApplication.java
@@ -4,8 +4,14 @@ import java.security.Principal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 @SpringBootApplication
 @RestController
@@ -16,5 +22,18 @@ public class GoogleserviceApplication {
    @RequestMapping(value = "/user")
    public Principal user(Principal principal) {
       return principal;
+   }
+   
+   @RequestMapping(value="/inicio", method=RequestMethod.GET)
+	public ModelAndView inicio(Model model, Principal principal) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("inicio");
+		model.addAttribute("perfil", principal);
+		return mav;
+	}
+   
+   @GetMapping("/logout-success")
+   public RedirectView redirectWithUsingRedirectView(RedirectAttributes attributes) {
+       return new RedirectView("index");
    }
 }
